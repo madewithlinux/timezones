a4f1848d0995a5cf9e7136189fe00f1471aba91f
Josh <madewithlinux.josh@gmail.com>
2021-06-13 19:45:18 -0500

    Updates

--- dist/index.js
+++ dist/index.js
@@ -10903,6 +10903,7 @@ function create_fragment2(ctx) {
       div = element("div");
       div.innerHTML = `<h1>helpful reference information</h1> 
   <ul><li class="svelte-y8zofw"><a target="_blank" rel="noreferrer noopener" href="https://www.google.com/search?q=how+to+set+timezone+in+windows+10">how to set timezone in windows 10</a></li> 
+    <li class="svelte-y8zofw"><a target="_blank" rel="noreferrer noopener" href="https://www.google.com/search?q=how+to+set+timezone+in+outlook">how to set timezone in outlook</a></li> 
     <li class="svelte-y8zofw"><a target="_blank" rel="noreferrer noopener" href="https://en.wikipedia.org/wiki/List_of_tz_database_time_zones">List of tz database time zones</a></li> 
     <li class="svelte-y8zofw"><a target="_blank" rel="noreferrer noopener" href="https://github.com/madewithlinux/timezones/">source code of this page</a></li></ul>`;
     },
